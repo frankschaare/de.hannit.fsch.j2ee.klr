cb5a610b67f0d1e8c2a5fbe4e8111387444ba0c3
hit <frank.schaare@gmail.com>
2017-03-24 14:29:58 +0100

    finished several Bugfixes

--- KLRWeb/src/de/hannit/fsch/klr/model/organisation/Organisation.java
+++ KLRWeb/src/de/hannit/fsch/klr/model/organisation/Organisation.java
@@ -3,22 +3,34 @@
  */
 package de.hannit.fsch.klr.model.organisation;
 
+import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.application.ProjectStage;
+import javax.faces.context.FacesContext;
 
+import de.hannit.fsch.common.MonatsSummen;
 import de.hannit.fsch.klr.ejb.beans.ApplicationStart;
+import de.hannit.fsch.klr.model.Datumsformate;
 import de.hannit.fsch.klr.model.kostenrechnung.KostenStelle;
 import de.hannit.fsch.klr.model.kostenrechnung.KostenTraeger;
+import de.hannit.fsch.klr.model.mitarbeiter.GemeinKosten;
+import de.hannit.fsch.klr.model.mitarbeiter.PersonalDurchschnittsKosten;
 import de.hannit.fsch.klr.model.mitarbeiter.Tarifgruppe;
 import de.hannit.fsch.klr.model.mitarbeiter.Tarifgruppen;
 import de.hannit.fsch.klr.model.mitarbeiter.Vorstand;
 import de.hannit.fsch.klr.model.team.Team;
 import de.hannit.fsch.klr.persistence.entities.LogaDatensatz;
 import de.hannit.fsch.klr.persistence.entities.Mitarbeiter;
+import de.hannit.fsch.klr.web.beans.ArbeitszeitanteilMBean;
 import de.hannit.fsch.klr.web.beans.MitarbeiterMBean;
+import de.hannit.fsch.util.DateUtility;
 
 
 /**
@@ -31,6 +43,11 @@ public class Organisation implements IOrganisation
 private ApplicationStart appStart;
 
 private final String name = "Hannoversche Informationstechnologien";
+private final static Logger log = Logger.getLogger(Organisation.class.getSimpleName());	
+private String logPrefix = null;	
+private FacesContext fc = null;
+private FacesMessage msg = null;
+private String detail = null;
 private TreeMap<Date, Monatsbericht> monatsBerichte = new TreeMap<Date, Monatsbericht>(); 
 private LocalDate abrechnungsMonat = null;
 private TreeMap<Integer, MitarbeiterMBean> mitarbeiterPNR = null;
@@ -39,8 +56,17 @@ private TreeMap<Integer, Team> teams = null;
 private TreeMap<Integer, KostenStelle> kostenstellen = null;
 private TreeMap<Integer, KostenTraeger> kostentraeger = null;
 private Tarifgruppen tarifgruppen = new Tarifgruppen();
+private MonatsSummen mSumme = null;
 
 private Vorstand vorstand = null;
+/**
+ * Wieviel Vollzeitanteile wurden aus den Tarifgruppen verteilt ?
+ */
+private double vzaeVerteilt = 0;
+/**
+ * Wie hoch ist die Summe der in den Mitarbeiterdaten gespeicherten Bruttoaufwendungen ?
+ */
+private double vzaeTotal = 0;	
 
 	/**
 	 * 
@@ -49,9 +75,16 @@ private Vorstand vorstand = null;
 	{
 	vorstand = new Vorstand();
 	mitarbeiterPNR = new TreeMap<>();
-	MitarbeiterMBean neu = null;
 	setAbrechnungsMonat(appStart.getMaxDate());
+	loadData();
+	}
 	
+	public void loadData()
+	{
+	fc = FacesContext.getCurrentInstance();	
+	logPrefix = this.getClass().getName() + ".loadData(): ";		
+	MitarbeiterMBean neu = null;
+		
 		for (Mitarbeiter entity : appStart.getMitarbeiter()) 
 		{
 		neu = new MitarbeiterMBean(entity);	
@@ -62,7 +95,90 @@ private Vorstand vorstand = null;
 	setTarifgruppen(new Tarifgruppen());
 	getTarifgruppen().setBerichtsMonat(appStart.getMaxDate());
 	getTarifgruppen().setAnzahlMitarbeiter(getMitarbeiterNachPNR().size());
+	
+	/*
+	 * Nachdem die Tarifgruppen erstellt wurden, wird für jeden Mitarbeiter
+	 * das passende Vollzeitäquivalent gespeichert:
+	 */
+	vzaeVerteilt = 0;
+		Tarifgruppe t = null;
+		for (MitarbeiterMBean m : getMitarbeiterNachPNR().values())
+		{
+		t = tarifgruppen.getTarifGruppen().get(m.getTarifGruppe());	
+		vzaeVerteilt += m.setVollzeitAequivalent(t.getVollzeitAequivalent());
+		}
+	if (fc.isProjectStage(ProjectStage.Development)) {log.log(Level.INFO, logPrefix + "Für den Monat " + Datumsformate.MONATLANG_JAHR.format(getAbrechnungsMonat()) + " wurden insgesamt " + NumberFormat.getCurrencyInstance().format(vzaeVerteilt) + " Vollzeitäquivalente auf " + getMitarbeiterNachPNR().size() + " Mitarbeiter verteilt.");}	
+		
+	/*
+	 * Im Log wird nun zu Prüfzwecken ausgegeben, wie hoch das Vollzeitäquivalent Insgesamt beträgt:	
+	 */
+	vzaeTotal = 0;	
+		for (MitarbeiterMBean m : getMitarbeiterNachPNR().values())
+		{
+			for (ArbeitszeitanteilMBean azv : m.getAzvMonat())
+			{
+			vzaeTotal += azv.getBruttoAufwand();	
+			}
+		}
+		if (NumberFormat.getCurrencyInstance().format(vzaeTotal).equals(NumberFormat.getCurrencyInstance().format(vzaeVerteilt)))
+		{
+		log.log(Level.INFO, logPrefix + "Für den Monat " + Datumsformate.MONATLANG_JAHR.format(getAbrechnungsMonat()) + " wurden insgesamt " + NumberFormat.getCurrencyInstance().format(vzaeTotal) + " Vollzeitäquivalente entsprechend der Arbeitszeitanteile verteilt.");	
+		}
+		else
+		{
+		log.log(Level.SEVERE, logPrefix + "Für den Monat " + Datumsformate.MONATLANG_JAHR.format(getAbrechnungsMonat()) + " wurden insgesamt " + NumberFormat.getCurrencyInstance().format(vzaeTotal) + " Vollzeitäquivalente entsprechend der Arbeitszeitanteile verteilt.");
+		}	
+		
+	/*
+	 * Nun steht das Vollzeitäquivalent für jeden Mitarbeiter fest.
+	 * Die Mitarbeiter werden erneut durchlaufen und es werden die Monatssummen für alle
+	 * gemeldeten Kostenstellen / Kostenträger gebildet	
+	 */
+	mSumme = new MonatsSummen();
+	mSumme.setGesamtSummen(getMitarbeiterNachPNR());
+	mSumme.setBerichtsMonat(DateUtility.asDate(getAbrechnungsMonat()));
+	
+	PersonalDurchschnittsKosten pdk = new PersonalDurchschnittsKosten(DateUtility.asDate(getAbrechnungsMonat()));
+	pdk.setMitarbeiter(getMitarbeiterNachPNR());
+	
+	GemeinKosten gk = new GemeinKosten(DateUtility.asDate(getAbrechnungsMonat()));
+	gk.setMitarbeiter(getMitarbeiterNachPNR());		
+	
+	/*
+	 * Nachdem alle Kostenstellen / Kostenträger verteilt sind, wird die Gesamtsumme gebildet und im Log ausgegeben.
+	 * Diese MUSS gleich dem Gesamtbruttoaufwand sein !
+	 */
+	double monatssummenTotal = mSumme.getKstktrMonatssumme();
+	
+	if (NumberFormat.getCurrencyInstance().format(monatssummenTotal).equals(NumberFormat.getCurrencyInstance().format(vzaeVerteilt)))
+	{
+	mSumme.setChecked(true);
+	mSumme.setSummeOK(true);
+
+	pdk.setChecked(true);
+	pdk.setDatenOK(true);
+	gk.setChecked(true);
+	gk.setDatenOK(true);
+	if (fc.isProjectStage(ProjectStage.Development)) {log.log(Level.INFO, logPrefix + "Für den Monat " + Datumsformate.MONATLANG_JAHR.format(getAbrechnungsMonat()) + " wurden insgesamt " + NumberFormat.getCurrencyInstance().format(monatssummenTotal) + " auf " + mSumme.getGesamtKosten().size() + " Kostenstellen / Kostenträger verteilt.");}
+	detail = "Für den Monat " + Datumsformate.MONATLANG_JAHR.format(getAbrechnungsMonat()) + " wurden insgesamt " + NumberFormat.getCurrencyInstance().format(vzaeVerteilt) + " auf " + mSumme.getGesamtKosten().size() + " Kostenstellen / Kostenträger verteilt. Das entspricht der Summe der Personalaufwendungen i.H.v. " + NumberFormat.getCurrencyInstance().format(monatssummenTotal); 
+	msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Monatssummen erfolgreich geprüft.", detail);
+	fc.addMessage(null, msg);
 	}
+	else
+	{
+	mSumme.setChecked(true);
+	mSumme.setSummeOK(false);
+	
+	pdk.setChecked(true);
+	pdk.setDatenOK(false);
+	gk.setChecked(true);
+	gk.setDatenOK(false);
+	detail = "Für den Monat " + Datumsformate.MONATLANG_JAHR.format(getAbrechnungsMonat()) + " wurden insgesamt " + NumberFormat.getCurrencyInstance().format(vzaeVerteilt) + " auf " + mSumme.getGesamtKosten().size() + " Kostenstellen / Kostenträger verteilt. Die Summe der Personalaufwendungen beträgt aber " + NumberFormat.getCurrencyInstance().format(monatssummenTotal); 
+	log.log(Level.SEVERE, logPrefix + detail);		
+	msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Monatssummen sind fehlerhaft !", detail);
+	fc.addMessage(null, msg);
+	}		
+	} 
 	
 	public void setTarifgruppen(Tarifgruppen tarifgruppen) 
 	{
@@ -166,7 +282,8 @@ private Vorstand vorstand = null;
 	return vorstand;
 	}
 
-
+	public MonatsSummen getmSumme() {return mSumme;}
+	public void setmSumme(MonatsSummen mSumme) {this.mSumme = mSumme;}
 	public Tarifgruppen getTarifgruppen() {return tarifgruppen;}
 	public LocalDate getAbrechnungsMonat() {return abrechnungsMonat;}
 	public void setAbrechnungsMonat(LocalDate abrechnungsMonat) {this.abrechnungsMonat = abrechnungsMonat;}
--- KLRWeb/src/de/hannit/fsch/klr/web/beans/IndexBean.java
+++ KLRWeb/src/de/hannit/fsch/klr/web/beans/IndexBean.java
@@ -1,11 +1,19 @@
 package de.hannit.fsch.klr.web.beans;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.ListDataModel;
 
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+
+import de.hannit.fsch.klr.model.kostenrechnung.Kostenrechnungsobjekt;
+import de.hannit.fsch.klr.model.mitarbeiter.Tarifgruppe;
 import de.hannit.fsch.klr.model.organisation.Organisation;
+import de.hannit.fsch.klr.model.team.Team;
 
 @ManagedBean
 @SessionScoped
@@ -13,7 +21,13 @@ public class IndexBean implements Serializable
 {
 private static final long serialVersionUID = 1L;
 private Organisation hannit = null;
-
+private ListDataModel<Kostenrechnungsobjekt> gesamt = null;
+private ListDataModel<Kostenrechnungsobjekt> gesamtKTR = null;
+private ListDataModel<Kostenrechnungsobjekt> gesamtKST = null;
+private ListDataModel<Tarifgruppe> tarifgruppenListe = null;
+private TreeNode root;
+private TreeNode treeName;
+private TreeNode treeTeams;
 
 	/**
 	 * Die Organisation wird hier initiert. Dadurch werden:
@@ -24,7 +38,83 @@ private Organisation hannit = null;
 	public IndexBean() 
 	{
 	hannit = new Organisation();
+	
+	gesamt = new ListDataModel<Kostenrechnungsobjekt>(new ArrayList<Kostenrechnungsobjekt>(hannit.getmSumme().getGesamtKosten().values()));
+	gesamtKTR = new ListDataModel<Kostenrechnungsobjekt>(new ArrayList<Kostenrechnungsobjekt>(hannit.getmSumme().getGesamtKostentraeger().values()));
+	gesamtKST = new ListDataModel<Kostenrechnungsobjekt>(new ArrayList<Kostenrechnungsobjekt>(hannit.getmSumme().getGesamtKostenstellen().values()));
+	tarifgruppenListe = new ListDataModel<Tarifgruppe>(new ArrayList<Tarifgruppe>(hannit.getTarifgruppen().getTarifGruppen().values()));
 
+	setTree();
 	}
+	
+	@SuppressWarnings("unused")
+	private void setTree() 
+	{
+    root = new DefaultTreeNode("Root", null);
+    treeName = new DefaultTreeNode("Root", null);
+    treeTeams = new DefaultTreeNode("Root", null);
 
+		TreeNode tNode = null;
+       	TreeNode mNode = null;
+    	TreeNode azvNode = null;
+		for (MitarbeiterMBean m : hannit.getMitarbeiterNachPNR().values())
+		{
+		mNode  = new DefaultTreeNode(m.getTyp(), m, root);
+			for (ArbeitszeitanteilMBean	azv : m.getAzvMonat()) 
+			{
+			azvNode = new DefaultTreeNode("AZV", azv, mNode);	
+			}
+			
+		}
+		
+    	mNode = null;
+    	azvNode = null;
+		for (MitarbeiterMBean m : hannit.getMitarbeiterNachName().values())
+		{
+		mNode  = new DefaultTreeNode(m.getTyp(), m, treeName);
+			for (ArbeitszeitanteilMBean	azv : m.getAzvMonat()) 
+			{
+			azvNode = new DefaultTreeNode("AZV", azv, mNode);	
+			}
+			
+		}
+		
+    	mNode = null;
+    	azvNode = null;
+		for (Team t : hannit.getTeams().values())
+		{
+		tNode  = new DefaultTreeNode("Team", t, treeTeams);
+			for (MitarbeiterMBean m : t.getTeamMitglieder().values())
+			{
+			mNode  = new DefaultTreeNode(m.getTyp(), m, tNode);
+				for (ArbeitszeitanteilMBean	azv : m.getAzvMonat()) 
+				{
+				azvNode = new DefaultTreeNode("AZV", azv, mNode);	
+				}
+			}
+		}		
+	}
+	
+	public ListDataModel<Tarifgruppe> getTarifgruppenRows() 
+	{
+	return tarifgruppenListe;	
+	}
+	
+	public ListDataModel<Kostenrechnungsobjekt> getMonatsGesamtSummen() 
+	{
+	return gesamt;		
+	}
+	
+	public ListDataModel<Kostenrechnungsobjekt> getGesamtSummenRowsKST() 
+	{
+	return gesamtKST;	
+	}
+	
+	public ListDataModel<Kostenrechnungsobjekt> getGesamtSummenRowsKTR() 
+	{
+	return gesamtKTR;	
+	}		
+    public TreeNode getRoot() {return root;}	
+    public TreeNode getTreeName() {return treeName;}	
+    public TreeNode getTreeTeams() {return treeTeams;}
 }
